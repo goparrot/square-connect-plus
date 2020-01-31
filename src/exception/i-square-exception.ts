38e9ec494493c436e1a086e177523e0f93259332
Coroliov Oleg <1880059+ruscon@users.noreply.github.com>
2020-01-31 21:20:23 +0200

    feat: add SquareException.toObject() method
    Former-commit-id: d6e194dc664b6c94b67f0e3c0d40645b71cf1bd4
    Former-commit-id: ebe01ba4cd358073de72e2c8ed0d19a6bb8566e1

---
+++ src/exception/i-square-exception.ts
@@ -0,0 +1,12 @@
+import { ModelError } from 'square-connect';
+
+export interface ISquareException {
+    name: string;
+    statusCode: number;
+    message: string;
+    apiError: ModelError;
+    retries?: number;
+    url?: string;
+    method?: string;
+    requestArgs?: any;
+}
--- test/unit/utils/common.utils.spec.ts
+++
@@ -1,9 +0,0 @@
-import { mergeDeepProps } from '../../../src/utils';
-
-describe('common.utils (unit)', (): void => {
-    describe('#mergeDeepProps', (): void => {
-        it('should merge object props', async (): Promise<any> => {
-            return mergeDeepProps({ a: { b: 1 }, c: 1, d: 1 }, { a: { b: 2 }, c: 2, e: 2 }).should.be.deep.eq({ a: { b: 2 }, c: 2, d: 1, e: 2 });
-        });
-    });
-});
