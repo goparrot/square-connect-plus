08113a51ca6b7c15dd4fa720e7b7317c8ebb4fb4
Coroliov Oleg <coroliov.o@goparrot.ai>
2022-09-15 12:45:18 +0300

    feat: allow passing merchantId for logging
    Former-commit-id: 94dc9be9fc4f43e9e84c7f69e9869a65abd310a9
    Former-commit-id: 894ab3aabbaeac31e4edfeffc45a77309bcef06a

--- src/interface/ISquareClientConfig.ts
+++ src/interface/ISquareClientConfig.ts
@@ -2,8 +2,15 @@ import type { Configuration } from 'square';
 import type { ILogger } from '../logger';
 import type { IRetriesOptions } from './IRetriesOptions';
 
+export type LogContext = {
+    /** square merchant id */
+    merchantId?: string;
+    [key: string]: any;
+};
+
 export interface ISquareClientConfig {
     retry?: Partial<IRetriesOptions>;
     configuration?: Partial<Omit<Configuration, 'accessToken'>>;
     logger?: ILogger;
+    logContext?: LogContext;
 }
