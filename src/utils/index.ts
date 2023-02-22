c5fa182c206a2f9d7163162cd2ab24469607b798
Tudor Bostan <tudor.b@goparrot.ai>
2023-02-22 11:59:21 +0200

    feat(square): add support create square factory with custom square client
    Former-commit-id: 4534266625538e9a4fca0a39e7138f7650995437
    Former-commit-id: 388dca8014e4911a02221908a726c9c8331b07b6

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,4 @@ export * from './retry.utils';
 export * from './SquareDataMapper';
 export * from './helper';
 export * from './isSquareApiException';
+export * from './isRetryableSquareApiException';
